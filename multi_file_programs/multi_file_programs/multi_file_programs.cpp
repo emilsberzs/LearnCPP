74b3261691c336f3cf3e047213b3ea6369f69ccc
Emils Berzs <emilsberzs@hotmail.com>
2025-04-03 15:18:41 +0100

    Multi file with declaration and header inlude

--- multi_file_programs/multi_file_programs/multi_file_programs.cpp
+++ multi_file_programs/multi_file_programs/multi_file_programs.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "multiply.h" //include multiply header file
 
 int add(int a, int b);//Declare the add() function so main knoiws that its somewhere
 
 int main()
 {
-	std::cout << add(2, 2);
+	std::cout << add(2, 2) << '\n';
+	std::cout << multiply(3, 3) << '\n';
 	return 0;
 }
---
+++ multi_file_programs/multi_file_programs/multiply.h
@@ -0,0 +1,2 @@
+#pragma once
+int multiply(int a, int b);//we just declare the head of function
