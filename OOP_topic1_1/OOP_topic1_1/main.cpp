1ff9ab454429e9973420a8bc13afe4c10c274508
Emils Berzs <emilsberzs@hotmail.com>
2025-04-16 07:53:20 +0100

    Menu Outline

---
+++ OOP_topic1_1/OOP_topic1_1/main.cpp
@@ -0,0 +1,12 @@
+// OOP_topic1_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Endline is cross-platform." << std::endl;
+    return 0;
+
+}
+
